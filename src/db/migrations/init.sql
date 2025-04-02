40d608e77594ae706e750cf1431d28c4c2ac8ea9
Iago Nuvem <iagonuvem@hotmail.com>
2025-04-01 22:08:39 -0300

    FEAT: Db migration & Postman collection

--- src/db/migrations/init.sql
+++ src/db/migrations/init.sql
@@ -1,3 +1,9 @@
+-- DROP tables in correct order
+DROP TABLE IF EXISTS resources;
+DROP TABLE IF EXISTS topics;
+DROP TABLE IF EXISTS users;
+
+-- Create tables
 CREATE TABLE IF NOT EXISTS topics (
   id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
@@ -25,6 +31,18 @@ CREATE TABLE IF NOT EXISTS resources (
   FOREIGN KEY (topicId) REFERENCES topics(id) ON DELETE CASCADE
 );
 
-INSERT INTO users (id, name, email, role, createdAt) VALUES (1, 'Admin', 'admin@email.com', 'Admin', '2025-04-01T10:00:00.000Z');
-INSERT INTO users (id, name, email, role, createdAt) VALUES (2, 'Editor', 'editor@email.com', 'Editor', '2025-04-01T10:00:00.000Z');
-INSERT INTO users (id, name, email, role, createdAt) VALUES (3, 'Viewer', 'viewer@email.com', 'Viewer', '2025-04-01T10:00:00.000Z')
+-- POPULATE USERS
+INSERT INTO users (id, name, email, role, createdAt) VALUES
+  ('1', 'Admin', 'admin@email.com', 'Admin', '2025-04-01T10:00:00.000Z'),
+  ('2', 'Editor', 'editor@email.com', 'Editor', '2025-04-01T10:00:00.000Z'),
+  ('3', 'Viewer', 'viewer@email.com', 'Viewer', '2025-04-01T10:00:00.000Z');
+
+-- POPULATE TOPICS
+INSERT INTO topics (id, name, parentTopicId, createdAt, versions) VALUES
+  ('t1', 'Programming', NULL, '2025-04-01T10:00:00.000Z', '[{"version":1,"content":"Programming is nice!","updatedAt":"2025-04-01T10:00:00.000Z"}]'),
+  ('t2', 'AI', 't1', '2025-04-01T10:00:00.000Z', '[{"version":1,"content":"AI will conquer the world","updatedAt":"2025-04-01T10:00:00.000Z"}]');
+
+-- Inserção de recursos
+INSERT INTO resources (id, topicId, url, description, type, createdAt, updatedAt) VALUES
+  ('r1', 't1', 'https://example.com/intro', 'Intro to programming', 'article', '2025-04-01T10:00:00.000Z', '2025-04-01T10:00:00.000Z'),
+  ('r2', 't2', 'https://example.com/ai', 'AI for begginers', 'video', '2025-04-01T10:00:00.000Z', '2025-04-01T10:00:00.000Z');
